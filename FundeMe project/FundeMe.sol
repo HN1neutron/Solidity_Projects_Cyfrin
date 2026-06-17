7f63d063efc890a8b8472fe7e3ce48ca5f9c8881
HN1neutron <hendyfastcorp@gmail.com>
2026-06-18 02:09:09 +0300

    Update FundeMe.sol
    project wrap up

--- FundeMe project/FundeMe.sol
+++ FundeMe project/FundeMe.sol
@@ -7,23 +7,60 @@ pragma solidity ^0.8.34;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5 * (10 ** 18) ;
+    uint256 public constant MINIMUM_USD = 5 * (10 ** 18) ;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public immutable i_owner;
+    
+    constructor () {
+        i_owner = msg.sender;
+
+    }
+
     function fund() public payable{
 
-        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH"); //1e18 = 1ETH = 1*10**18 / (1*10^18)
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH"); //1e18 = 1ETH = 1*10**18 / (1*10^18)
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
    
     }
 
-    // function withdraw() public {}
+    function withdraw() public onlyOwner {
+        
+        // for loop- starting index, ending index, step amount
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address [](0);
+        // to withdraw funds- transfer, send, call
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
 
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner, "Sender is not owner!");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+ 
 }
+// constant and immutable are keywords that make it so that your variables cant be changed
